6dfba7dc891d0bfa60b7d46713812329d9af5943
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2023-10-14 23:11:12 +0530

    LandingFinal

---
+++ src/components/Features.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+function Features() {
+  return (
+    <section className="pt-24 bg-gray-900 h-full  w-full pb-20">
+      <div className="flex flex-col items-center">
+  <div className="mt-4 grid grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 w-3/4">
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700 ">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400 min-w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+        </svg>
+      </div>
+
+      <div className="ml-6">
+        <h2 className="font-semibold text-xl ">Create personality for your NFTs <br/><br/></h2>
+        <p className="mt-2 text-sm text-gray-500">Infuse your NFTs with unique character and charm. Give your digital assets a distinct personality by customizing their attributes, histories, and traits. Make your NFTs stand out in the digital crowd by shaping their identity in the metaverse.</p>
+      </div>
+    </div>
+
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="#60a5fa" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+        </svg>
+      </div>
+
+      <div className="ml-4">
+        <h2 className="font-semibold text-xl">Share Your Thoughts from the POV of an NFT</h2>
+        <p className="mt-2 text-sm text-gray-500">Step into the virtual shoes of your NFTs and share their digital adventures, experiences, and stories. Let your NFTs speak for themselves and engage with the world from their unique perspective. Explore the creative potential of your NFTs and share their virtual voice.</p>
+      </div>
+    </div>
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="#60a5fa" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+        </svg>
+      </div>
+
+      <div className="ml-4">
+        <h2 className="font-semibold text-xl">Form Social Connections with Your NFTs</h2>
+        <p className="mt-2 text-sm text-gray-500">Forge meaningful bonds with your digital assets. Connect with your NFTs on a personal level, building a sense of community within the metaverse. Share in their triumphs and experiences as if they were your digital companions</p>
+      </div>
+    </div>
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="#60a5fa" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+        </svg>
+      </div>
+
+      <div className="ml-4">
+        <h2 className="font-semibold text-xl">Get Your Latest Notifications Seamlessly</h2>
+        <p className="mt-2 text-sm text-gray-500">Stay effortlessly informed in the fast-paced world of NFTs. Receive real-time updates, news, and messages that matter most to you. Our platform ensures you're always in the know, making your NFT journey smoother and more engaging.</p>
+      </div>
+    </div>
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="#60a5fa" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+        </svg>
+      </div>
+
+      <div className="ml-4">
+        <h2 className="font-semibold text-xl">Be the Next NFT Influencer <br/><br/></h2>
+        <p className="mt-2 text-sm text-gray-500">Become a trendsetter, setting the bar for NFT appreciation and influence in the metaverse</p>
+      </div>
+    </div>
+    <div className="flex items-start rounded-xl bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-shrink-0 h-12 w-12 items-center justify-center rounded-full border border-gray-700">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="#60a5fa" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+        </svg>
+      </div>
+
+      <div className="ml-4">
+        <h2 className="font-semibold text-xl">Generate Passive Revenue by Holding an NFT</h2>
+        <p className="mt-2 text-sm text-gray-500">Turn your NFT holdings into a source of income without active trading. Your NFTs work for you, earning you passive revenue through various mechanisms . Maximize the potential of your digital assets and let them work for your financial success</p>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+  )
+}
+
+export default Features
