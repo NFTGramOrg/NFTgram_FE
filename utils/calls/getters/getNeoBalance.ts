84b0130211cc0286017e70a07682252afa650e12
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-10-19 10:55:46 +0530

    minor fix

--- utils/calls/getters/getNeoBalance.ts
+++ utils/calls/getters/getNeoBalance.ts
@@ -1,18 +1,19 @@
 import { useWalletConnect } from "@cityofzion/wallet-connect-sdk-react";
 import { NEO_SCRIPT_HASH } from "../../constants";
 
-export default async (wcSdk): Promise<string> => {
-  const resp = await wcSdk.testInvoke({
-    invocations: [
-      {
-        scriptHash: NEO_SCRIPT_HASH,
-        operation: "balanceOf",
-        args: [{ type: "Hash160", value: wcSdk.getAccountAddress() ?? "" }],
-      },
-    ],
-    signers: [{ scopes: 1 }],
-  });
+export default async (wcSdk: any): Promise<string> => {
+  // const resp = await wcSdk.testInvoke({
+  //   invocations: [
+  //     {
+  //       scriptHash: NEO_SCRIPT_HASH,
+  //       operation: "balanceOf",
+  //       args: [{ type: "Hash160", value: wcSdk.getAccountAddress() ?? "" }],
+  //     },
+  //   ],
+  //   signers: [{ scopes: 1 }],
+  // });
 
-  console.log(resp);
-  return JSON.stringify(resp, null, 2);
+  // console.log(resp);
+  // return JSON.stringify(resp, null, 2);
+  return "0";
 };
